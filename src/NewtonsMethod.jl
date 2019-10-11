ebaf74e7bd410b40e6c916137f2f4b62da5f3354
PooyaFa <pooyaravari@gmail.com>
2019-10-11 13:02:35 -0700

    update

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -4,14 +4,19 @@ greet() = print("Hello World!")
 
 using LinearAlgebra, ForwardDiff
 
-function fixedpointmap(f, f_prime, x_0; tolerance=1E-7, maxiter=1000)
+function fixedpointmap(f, f_prime, x_0; tolerance=1E-12, maxiter=1000)
+    # setup the algorithm
+#     x_old = iv
     normdiff = Inf
     iter = 1
+    if f(x_0) == 0
+        return (value = x_0, normdiff=0, iter=0)
+    end
     while normdiff > tolerance && iter <= maxiter
         x_new = x_0 - f(x_0)/f_prime(x_0) # use the passed in map
         if norm(x_new - x_0) > tolerance && iter > 100
             println("non-convergence ")
-            return
+            return #(value = nothing, normdiff=nothing, iter=iter)
         end
         normdiff = norm(x_new - x_0)
         x_0 = x_new
@@ -22,9 +27,12 @@ end
 
 
 D(f) = x -> ForwardDiff.derivative(f, x)
-function fixedpointmap(f, x_0; tolerance=1E-7, maxiter=1000)
+function fixedpointmap(f, x_0; tolerance=1E-12, maxiter=1000)
     f_prime = D(f)
-    x_0, normdiff, iter = fixedpointmap(f, f_prime, x_0)
+    if nothing == fixedpointmap(f, f_prime, x_0)
+        return
+    end
+    x_0, normdiff, iter = fixedpointmap(f, f_prime, x_0, tolerance=tolerance, maxiter=maxiter)
     return (value = x_0, normdiff=normdiff, iter=iter)
 end
 
