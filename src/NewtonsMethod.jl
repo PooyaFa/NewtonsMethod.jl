537f7450b8989b64631a272a0b70750c94b65c62
PooyaFa <pooyaravari@gmail.com>
2019-10-11 11:11:16 -0700

    adding functions

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -2,4 +2,30 @@ module NewtonsMethod
 
 greet() = print("Hello World!")
 
+using LinearAlgebra, ForwardDiff
+
+function fixedpointmap(f, f_prime, x_0; tolerance=1E-7, maxiter=1000)
+    # setup the algorithm
+#     x_old = iv
+    normdiff = Inf
+    iter = 1
+    while normdiff > tolerance && iter <= maxiter
+        x_new = x_0 - f(x_0)/f_prime(x_0) # use the passed in map
+        normdiff = norm(x_new - x_0)
+        x_0 = x_new
+        iter = iter + 1
+    end
+    return (value = x_0, normdiff=normdiff, iter=iter) # A named tuple
+end
+
+
+D(f) = x -> ForwardDiff.derivative(f, x)
+function fixedpointmap(f, x_0; tolerance=1E-7, maxiter=1000)
+    f_prime = D(f)
+    x_0, normdiff, iter = fixedpointmap(f, f_prime, x_0)
+    return (value = x_0, normdiff=normdiff, iter=iter)
+end
+
+export fixedpointmap
+
 end # module
